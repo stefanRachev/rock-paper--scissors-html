433b64c87b178c012af1e1d19f2cb8320e7657be
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-01-25 17:52:57 +0200

    Initial commit

---
+++ aps.js
@@ -0,0 +1,42 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice
+let computerChoice 
+let result
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener("click",(e)=>{
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber === 1){
+        computerChoice = "rock"
+    }
+    if(randomNumber === 2){
+        computerChoice = "paper"
+    }
+    if(randomNumber === 3){
+        computerChoice = "scissors"
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+function getResult(){
+    if ((userChoice === "rock" && computerChoice === "scissors")
+        || (userChoice === "paper" && computerChoice === "rock")
+        || (userChoice === "scissors" && computerChoice === "paper")) {
+        result = "You win !"
+    } else if ((computerChoice === "rock" && userChoice === "scissors")
+        || (computerChoice === "paper" && userChoice === "rock")
+        || (computerChoice === "scissors" && userChoice === "paper")){
+            result = "You lose !"
+        }else{
+            result = "Its a draw !"
+        }
+        resultDisplay.innerHTML = result
+}
